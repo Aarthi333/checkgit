13271642c27a07f2d97b2d18b7f42ba446515a86
Aarthi333 <82799431+Aarthi333@users.noreply.github.com>
2021-05-12 22:05:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Day19
+{
+    class Program
+    {
+        int[] feedbackScores = { 45, 99, 87, 67, 56, 42, 32, 47, 1 }; 
+
+        void PrintLowFeedbackCount()
+        {
+            //using linq in collections
+            var Count = feedbackScores.Where(score => score < 60).Count();   //passing a predicate instead of query syntax(from n ...)
+            Console.WriteLine("the number of feedbacks less than 60 is " +Count);
+
+        }
+        void PrintFeedbackInAscendingOrder()
+        {
+            var SortedFeedback = feedbackScores.OrderBy(score => score);
+            foreach (var item in SortedFeedback)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new Program().PrintLowFeedbackCount();
+            new Program().PrintFeedbackInAscendingOrder();
+        }
+    }
+}
